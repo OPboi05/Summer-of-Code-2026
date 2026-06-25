7161984c99f80f7c5c88ecbf58220bea2bc92d6d
Harshal Gupta <harshalg051007@gmail.com>
2026-06-25 23:08:16 +0530

    Final signal generating file

---
+++ signal_generator.py
@@ -0,0 +1,12 @@
+# In this file I have only focused on signal generation and not on size of the position
+import indicator 
+import filters
+
+choices = []
+for i in range(50,len(arr)): # I start from 50 to give EMA, RSI etc enough run-up
+    if (signal(aud,i) and RSI(aud,i)>=70):
+        choices.append("BUY")
+    elif (signal(aud,i) and RSS(aud,i)<=30):
+        choices.append("SELL")
+    else:
+        choices.append("HOLD")
